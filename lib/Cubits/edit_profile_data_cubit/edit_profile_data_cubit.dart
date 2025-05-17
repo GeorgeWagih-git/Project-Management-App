6f3d7b0e78842bad2d6054e163c4898c776e0a1d
Kyrillos <koko10.cr7@gmail.com>
2025-05-17 04:40:01 +0200

    edit profile cubit commit

---
+++ lib/Cubits/edit_profile_data_cubit/edit_profile_data_cubit.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_application_1/Classes/user_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<bool> updateUserOnServer(UserModel user) async {
+  final url = Uri.parse("https://frankly-refined-escargot.ngrok-free.app/api/User/Edit");
+  final prefs = await SharedPreferences.getInstance();
+  final token = prefs.getString('token');
+
+  final body = jsonEncode(user.toJson());
+  print("PUT $url");
+  print("Token: $token");
+  print("Body: $body");
+
+  final response = await http.put(
+    url,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token',
+    },
+    body: body,
+  );
+
+  print("Status Code: ${response.statusCode}");
+  print("Response Body: ${response.body}");
+
+  return response.statusCode == 200;
+}
+
