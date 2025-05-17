74bd9574ec531906a3a4b482de3c07506022b780
Kyrillos <koko10.cr7@gmail.com>
2025-05-17 04:41:23 +0200

    chat cubit commit

---
+++ lib/Cubits/chat_cubit/chat_cubit.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../Classes/message_model.dart';
+import '../../widgets/chat_service.dart';
+
+part 'chat_state.dart';
+
+class ChatCubit extends Cubit<ChatState> {
+  final ChatService chatService;
+
+  ChatCubit(this.chatService) : super(ChatInitial());
+
+  Future<void> loadMessages({
+    required String token,
+    required String receiverId,
+  }) async {
+    emit(ChatLoading());
+    try {
+      final messages = await chatService.loadMessages(
+        token: token,
+        receiverId: receiverId,
+      );
+      emit(ChatLoaded(messages: messages));
+    } catch (e) {
+      emit(ChatError(message: e.toString()));
+    }
+  }
+
+  void sendMessage({
+    required String senderId,
+    required String receiverId,
+    required String message,
+  }) {
+    chatService.sendMessage(
+      senderId: senderId,
+      receiverId: receiverId,
+      message: message,
+    );
+  }
+}
